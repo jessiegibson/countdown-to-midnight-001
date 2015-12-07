b32ee57885e7431bd994fb235ac0810e34c9bf5c
jessiegibson <jessie@thirstynyc.com>
2015-12-06 21:07:48 -0700

    Sleep

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,21 @@
 #write your code here
 
-def countdown
+def countdown(input)
+  x = input
+  while x > 0 
+    puts "#{x} SECOND(S)!"
+    x -= 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(input)
+ x = input
+  while x.to_i >= 5
+    sleep(5)
+    return true
+    x += 1
+  end
+  return false
+
 end
